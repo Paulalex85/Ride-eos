4a92421c8c4db78c61b9fd20f2dc50206408946a
jeanjack <tessier.paulalexandre@orange.fr>
2019-03-14 21:34:27 +0100

    add stackpower api

--- frontend/src/components/UserProfile/components/StackPower/StackPower.jsx
+++ frontend/src/components/UserProfile/components/StackPower/StackPower.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 // Components
-import { Col, Form, Row } from 'react-bootstrap';
+import { Col, Form, Row, Button } from 'react-bootstrap';
 
 import { StackpowerAction } from 'actions';
-import { ApiService } from 'services';
+import { ApiService, ApiServiceScatter } from 'services';
 
 class StackPower extends Component {
     constructor(props) {
@@ -13,28 +13,57 @@ class StackPower extends Component {
         super(props);
 
         this.getStackpower();
+
+        this.state = {
+            form: {
+                stack: "0.0000 SYS"
+            },
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.getStackpower = this.getStackpower.bind(this);
     }
 
     getStackpower() {
         const { setListStackpower, user: { account }, scatter: { scatter } } = this.props;
-
-        console.log(account)
 
         ApiService.getStackByAccount(account, scatter).then(stack => {
             setListStackpower({ listStackpower: stack, account: account });
         }).catch((err) => { console.error(err) });
     }
 
+    handleChange(event) {
+        const { name, value } = event.target;
+        const { form } = this.state;
+
+        this.setState({
+            form: {
+                ...form,
+                [name]: value,
+            },
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const { form: { stack } } = this.state;
+        const { scatter: { scatter } } = this.props;
+
+        return ApiServiceScatter.stackpow(stack, scatter).then(() => {
+            this.getStackpower();
+        }).catch((err) => { console.error(err) });
+    }
+
     render() {
         const { stackpower: { listStackpower } } = this.props;
+        const { form } = this.state;
 
-        let stack = undefined;
+        let balance = "0.0000 SYS";
         for (let i = 0; i < listStackpower.length; i++) {
             const element = listStackpower[i];
-            console.log(element)
-            console.log(new Date(element.endAssignment).getTime())
             if (new Date(element.endAssignment).getTime() === 0) {
-                stack = element;
+                balance = element.balance;
                 break;
             }
         }
@@ -42,15 +71,34 @@ class StackPower extends Component {
         return (
 
             <Form>
-                {stack !== undefined &&
-                    <Form.Group as={Row} className="justify-content-center" controlId="userStack">
-                        <Form.Label column md={2}>Stackpower balance</Form.Label>
-                        <Col md={2}>
-                            <Form.Label>{stack.balance}</Form.Label>
-                        </Col>
-                        <Col md={4}></Col>
-                    </Form.Group>
-                }
+
+                <Form.Group as={Row} className="justify-content-center" controlId="userStack">
+                    <Form.Label column md={2}>Stackpower balance</Form.Label>
+                    <Col md={2}>
+                        <Form.Label>{balance}</Form.Label>
+                    </Col>
+                    <Col md={4}></Col>
+                </Form.Group>
+
+                <Form.Group as={Row} className="justify-content-center" controlId="userStackAdd">
+                    <Form.Label column md={2}>Add Stackpower</Form.Label>
+                    <Col md={2}>
+                        <Form.Control
+                            type="text"
+                            name="stack"
+                            value={form.stack}
+                            onChange={this.handleChange}
+                        />
+
+                    </Col>
+                    <Col md={4}>
+                        <Button
+                            onClick={this.handleSubmit}
+                            variant='primary'>
+                            ADD
+                        </Button>
+                    </Col>
+                </Form.Group>
             </Form >
 
         )
