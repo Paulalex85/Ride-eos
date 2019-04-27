a59001179f96b9aa2f25458078c3a2495303f19d
Paul <tessier.paulalexandre@orange.fr>
2019-04-27 20:03:12 +0200

    add text and align new order

--- frontend/src/components/OrderDashboard/components/NewOrder/NewOrder.jsx
+++ frontend/src/components/OrderDashboard/components/NewOrder/NewOrder.jsx
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import { LinkContainer } from "react-router-bootstrap";
-import { Button } from 'react-bootstrap'
+import { Button, Col, Row, Container } from 'react-bootstrap'
 
 
 class NewOrder extends Component {
 
     render() {
         return (
-            <LinkContainer to="/create">
-                <Button
-                    variant='primary'>
-                    Create order
-                </Button>
-            </LinkContainer>
+            <Container className="mt-4 mb-5">
+                <Row className="justify-content-md-center">
+                    <Col className="col-md-auto">
+                        <h5>New order ?</h5>
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center mt-3">
+                    <Col className="col-md-auto">
+                        <LinkContainer to="/create">
+                            <Button
+                                variant='primary'>
+                                Create order
+                        </Button>
+                        </LinkContainer>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
